844ca351f01c5fc5e45813b6abc4f9ddd8e20ba9
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2019-03-04 17:26:02 +0100

    define HAVE_SYS_CLOSE_... for Pd>=0.43

--- mediasettings.h
+++ mediasettings.h
@@ -27,6 +27,12 @@
 # define BUILD_DATE "on " __DATE__ " at " __TIME__
 #endif
 
+#if (defined PD_MAJOR_VERSION && defined PD_MINOR_VERSION) && (PD_MAJOR_VERSION > 0 || PD_MINOR_VERSION >= 43)
+# define HAVE_SYS_CLOSE_MIDI 1
+# define HAVE_SYS_CLOSE_AUDIO 1
+#endif
+
+
 /**
  * find EOS of a string of tokens
  * where EOS might be indicated by \0, }
